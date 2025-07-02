62395dfdf87263aa89115b282c40edc399e8ce41
TEJAS SHINDE <tejasshindee2024@gmail.com>
2025-07-02 23:05:51 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import { serve } from '@hono/node-server';
 import { Hono } from 'hono';            
 import { prettyJSON } from 'hono/pretty-json';         
 import { serveStatic } from '@hono/node-server/serve-static';
-// import { sendMail } from '../services/mail.js';                                   
+// import { sendMail } from '../services/mail.js';                                                 
 import jwt from 'jsonwebtoken';                
 import dotenv from 'dotenv'                            
 import { auth } from 'hono/utils/basic-auth';                
